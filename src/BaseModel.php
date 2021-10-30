720f492760d3e3d898efc0dd1859c4b19a12a84b
Luis Gustavo Santarosa Pinto <gustavo-computacao@hotmail.com>
2021-10-30 13:15:05 -0300

    Ajustes

--- src/BaseModel.php
+++ src/BaseModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace GustavoSantarosa\LaravelToolPack;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
--- src/ServiceInterface.php
+++ src/ServiceInterface.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Services;
+namespace GustavoSantarosa\LaravelToolPack;
 
 use GustavoSantarosa\LaravelToolPack\DataTransferObject;
 
